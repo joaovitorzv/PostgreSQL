72f35e99385fb7b786d9015c74445d0ed39d5fc4
João vitor oliveira <resetgamer1@gmail.com>
2020-02-19 21:07:36 -0300

    project finished

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const userController = require('./controllers/userController');
 const addressController = require('./controllers/addressController');
+const techController = require('./controllers/techController');
+const reportController = require("./controllers/reportController");
 
 const routes = express.Router();
 
@@ -10,5 +12,11 @@ routes.get('/users', userController.index);
 
 routes.post('/users/:user_id/addresses', addressController.store);
 routes.get('/users/:user_id/addresses', addressController.index);
+
+routes.post('/users/:user_id/techs', techController.store);
+routes.get('/users/:user_id/techs', techController.index);
+routes.delete('/users/:user_id/techs', techController.delete);
+
+routes.get('/report', reportController.show);
 
 module.exports = routes;
